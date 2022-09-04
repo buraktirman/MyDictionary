86d14373a596d849468d631d0d45e73561d16d2c
buraktirman <buraktrman@gmail.com>
2022-09-04 23:16:41 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<T, K>
+    {
+        Tuple<T, K>[] items;
+
+        public MyDictionary()
+        {
+            items = new Tuple<T, K>[0];
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+
+        public void Add(T key, K value)
+        {
+            if (ControlKey(key))
+            {
+                Tuple<T, K>[] tempDictionary = items;
+                items = new Tuple<T, K>[items.Length + 1];
+
+                for (int i = 0; i < tempDictionary.Length; i++)
+                {
+                    items[i] = tempDictionary[i];
+                }
+
+                items[items.Length - 1] = new Tuple<T, K>(key, value);
+            }
+            else
+            {
+                Console.WriteLine("Key değeri daha önce kullanılmamış olmalıdır!");
+            }
+
+        }
+
+        public void ShowDictionary()
+        {
+            Console.WriteLine("-----Key : Value-----");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Item1 + " : " + item.Item2);
+            }
+        }
+
+        private bool ControlKey(T key)
+        {
+            bool flag = true;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Item1.ToString() == key.ToString())
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dict = new MyDictionary<string, int>();
+
+            dict.Add("Burak", 5);
+            dict.Add("Berkay", 3);
+            dict.Add("Burak", 8); // Hata mesajı verir ve sözlüğe eklemez çünkü aynı key'e sahip olamaz.
+            dict.Add("Samet", 3); // Ancak aynı değere sahip olabilir.
+
+            Console.WriteLine(dict.Count);
+
+            dict.ShowDictionary();
+
+        }
+    }
+}
